b334120340f6b033be38bf1d679dabd7de8192e4
Ben Warrington <ben@mysticalnomad.com>
2026-09-02 12:12:08 +0100

    Update site.js

--- site.js
+++ site.js
@@ -1,10 +1,18 @@
-const store = new SteinStore(
-    "https://api.steinhq.com/v1/storages/6a97f91792b1163e973b381e"
-);
-const loadData = () => {
-    store.read("LEGO").then(data => {
-        console.log(data);
-    });
-}
+// const store = new SteinStore(
+//     "https://api.steinhq.com/v1/storages/6a97f91792b1163e973b381e"
+// );
+// const loadData = () => {
+//     store.read("LEGO").then(data => {
+//         console.log(data);
+//     });
+// }
 
-loadData();
+const SHEET_ID = "1Nl4ENaosyt6OlxEag3RbeTxZ3opVgGK1cyOathAohQk";
+const API_KEY = "AIzaSyD8EVImOPu7MmNbxWib7q721vcnD1tBf4U";
+const RANGE = "LEGO";
+
+fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${RANGE}?key=${API_KEY}`)
+  .then(response => response.json())
+  .then(data => console.log(data.values));
+
+// loadData();
